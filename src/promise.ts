038a49692ffd549171f73c40f5befb5534889a0b
captain-tab <d491405879@gmail.com>
2021-09-21 18:36:30 +0800

    feat: create promise class

--- src/promise.ts
+++ src/promise.ts
@@ -1,5 +1,28 @@
 class Promise {
+  success = null
+  fail = null
 
+  resolve () {
+    setTimeout(()=>{
+      this.success()
+    },0)
+  }
+
+  reject () {
+    setTimeout(()=>{
+      this.fail()
+    },0)
+  }
+  constructor(fn: Function) {
+    if(typeof fn !== 'function') {
+      throw new Error('the parameter has to be Function type')
+    }
+    fn(this.resolve.bind(this), this.reject.bind(this))
+  }
+  then(success, fail) {
+    this.success = success
+    this.fail = fail
+  }
 }
 
 export default Promise
--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,4 @@
 import * as chai from 'chai';
-import { type } from 'os';
 
 const assert = chai.assert;
 import Promise from '../src/promise'
@@ -9,4 +8,44 @@ describe("Promise", ()=> {
     assert.isFunction(Promise)
     assert.isObject(Promise.prototype)
   })
+  it("params is function type", ()=> {
+    assert.throw(()=> {
+      // @ts-ignore
+      new Promise(1)
+    })
+    assert.throw(()=> {
+      // @ts-ignore
+      new Promise(false)
+    })
+  })
+  it('new Promise returns an object with then function inside', ()=> {
+    const promise = new Promise(()=> {})
+    assert.isFunction(promise.then)
+  })
+  it('new Promise excute fn function immediately', ()=> {
+    let called = false
+    const promise = new Promise((resolve, reject)=> {
+      called = true
+      assert.isFunction(resolve)
+      assert.isFunction(reject)
+    })
+    // @ts-ignore
+    assert(called === true)
+  })
+  it('Promise will excute success function after reslove has been called', (done)=> {
+    let called = false
+    const promise = new Promise((resolve, reject)=> {
+      assert(called === false)
+      resolve()
+      setTimeout(()=> {
+        assert(called === true)
+        done()
+      },0) 
+    })
+    // @ts-ignore
+    promise.then(()=> {
+      called = true
+    })
+  })
+
 });
