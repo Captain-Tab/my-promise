d2e6e4828e76cb88960b7bdf791f73e0d15f57a0
captain-tab <d491405879@gmail.com>
2021-09-22 23:29:56 +0800

    feat: continue build promise

--- src/promise.ts
+++ src/promise.ts
@@ -1,27 +1,52 @@
 class Promise {
-  success = null
-  fail = null
+  state = 'pending'
+  callbacks = []
 
-  resolve () {
-    setTimeout(()=>{
-      this.success()
-    },0)
-  }
 
-  reject () {
-    setTimeout(()=>{
-      this.fail()
-    },0)
-  }
   constructor(fn: Function) {
     if(typeof fn !== 'function') {
       throw new Error('the parameter has to be Function type')
     }
     fn(this.resolve.bind(this), this.reject.bind(this))
   }
-  then(success, fail) {
-    this.success = success
-    this.fail = fail
+
+  resolve (result) {
+    if(this.state !== 'pending') {
+      return
+    }
+    this.state = 'fulfilled'
+    setTimeout(()=>{
+      this.callbacks.forEach(handle => {
+        if(typeof handle[0] === 'function') {
+          handle[0].call(undefined, result)
+        }
+      })
+    },0)
+  }
+
+  reject (reason) {
+    if(this.state !== 'pending'){
+      return
+    }
+    this.state = 'rejected'
+    setTimeout(()=>{
+      this.callbacks.forEach(handle => {
+        if(typeof handle[1] === 'function') {
+          handle[1].call(undefined, reason)
+        }
+      })
+    },0)
+  }
+
+  then(success?, fail?) {
+    const handle = []
+    if(typeof success === 'function') {
+      handle[0] = success
+    }
+    if(typeof fail === 'function') {
+      handle[1] = fail
+    }
+    this.callbacks.push(handle)
   }
 }
 
--- test/index.ts
+++ test/index.ts
@@ -40,7 +40,7 @@ describe("Promise", ()=> {
       done()
     })
   })
-  it('Promise will excute success function after reslove has been called', (done)=> {
+  it('Promise will execute success function after resolve has been called', (done)=> {
     const success = sinon.fake()
     const promise = new Promise((resolve, reject)=> {
       assert.isFalse(success.called)
@@ -52,5 +52,95 @@ describe("Promise", ()=> {
     })
     // @ts-ignore
     promise.then(success)
+  })
+  it('Promise will execute fail function after reject has been called', (done)=> {
+    const fail = sinon.fake()
+    const promise = new Promise((resolve, reject)=> {
+      assert.isFalse(fail.called)
+      reject()
+      setTimeout(()=> {
+        assert.isTrue(fail.called)
+        done()
+      })
+    })
+    // @ts-ignore
+    promise.then(null,fail)
+  })
+  it('Promise.then receive function type parameter', ()=> {
+      const promise = new Promise(resolve=> {
+        resolve()
+      })
+    promise.then(false, null)
+    assert(1 === 1)
+  })
+  it('onFulfilled function will be called after promise done', (done)=> {
+    const succeed = sinon.fake()
+    const promise = new Promise(resolve=> {
+      assert.isFalse(succeed.called)
+      resolve(233)
+      resolve(234)
+      setTimeout(()=> {
+        assert(promise.state === 'fulfilled')
+        assert.isTrue(succeed.calledOnce)
+        assert(succeed.calledWith(233))
+        done()
+      }, 0)
+    })
+    promise.then(succeed)
+  })
+  it('onRejected function will be called after promise failed', (done)=> {
+    const fail = sinon.fake()
+    const promise = new Promise((resolve, reject)=> {
+      assert.isFalse(fail.called)
+      reject(233)
+      reject(234)
+      setTimeout(()=> {
+        assert(promise.state === 'rejected')
+        assert.isTrue(fail.calledOnce)
+        assert(fail.calledWith(233))
+        done()
+      }, 0)
+    })
+    promise.then(null, fail)
+  })
+  it(' 2.2.3 could not call then functions before code execute', (done)=> {
+    const succeed = sinon.fake()
+    const promise = new Promise((resolve)=> {
+      resolve()
+    })
+    promise.then(succeed)
+    console.log(1)
+    assert.isFalse(succeed.called)
+    setTimeout(()=> {
+      assert.isTrue(succeed.called)
+      done()
+    },0)
+  })
+  it('2.2.5 onFulfilled and onRejected must be called as functions', (done)=> {
+    const promise = new Promise(resolve => {
+      resolve()
+    })
+    promise.then(function (){
+      'use strict'
+      assert(this === undefined)
+      done()
+    })
+  })
+  it('2.2.6 then could be called multiple times by same promise', (done)=> {
+    const promise = new Promise(resolve => {
+      resolve()
+    })
+    const callbacks = [sinon.fake(), sinon.fake(), sinon.fake()]
+    promise.then(callbacks[0])
+    promise.then(callbacks[1])
+    promise.then(callbacks[2])
+    setTimeout(()=> {
+      assert(callbacks[0].called)
+      assert(callbacks[1].called)
+      assert(callbacks[2].called)
+      assert(callbacks[1].calledAfter(callbacks[0]))
+      assert(callbacks[2].calledAfter(callbacks[1]))
+      done()
+    }, 0)
   })
 });
